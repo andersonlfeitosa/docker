d4c33e46e58a7449d4df2a5fb3689bedfda1c150
Anderson Lobo Feitosa <andersonlf@gmail.com>
2017-12-08 01:51:37 -0200

    add new images

--- images_required.bash
+++ images_required.bash
@@ -3,6 +3,10 @@
 docker pull registry:2
 docker pull jenkins/jenkins:lts
 docker pull sonarqube:alpine
+docker pull sonarqube
 docker pull rabbitmq:3-management
 docker pull traefik
 docker pull openjdk:8-jre-alpine
+docker pull sonatype/nexus:oss
+docker pull centos:centos7
+
